01273b646658789b034e762536b71eed37b9d069
atul-dev-ai <paulatul020@gmail.com>
2026-05-12 13:40:31 +0600

    Notes added

---
+++ part-01-practices/phase-03/comperison.py
@@ -0,0 +1,22 @@
+print(10 > 5)
+a = 10
+b = 10
+
+
+"""
+equal : ==
+not equal : !=
+greater than : >=
+less than : 
+"""
+
+# value compare korar jonno 2ta = use korte hoy ==
+print("Eqaul to ", a == b)
+
+print("10" == 10) # ekhane ekta string arekta int ekhane False ashbe
+
+# Not equal (!=)
+print("Not equal to? ", 20 != b)
+
+print(a <= b)
+print(10 >= b)
