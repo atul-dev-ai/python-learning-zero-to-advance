4dd916c7a36e27262c9a8076c1720440dce99afe
atul-dev-ai <paulatul020@gmail.com>
2026-05-14 07:35:21 +0600

    function

---
+++ part-02-practices/phase-01/synt.py
@@ -0,0 +1,9 @@
+# def means define
+# def er por function name dite hobe tarpor backet use kore parameter use korte hobe.
+
+def greet (name):
+    print("Hello", name)
+greet("Atul")
+greet("Ankit")
+
+
