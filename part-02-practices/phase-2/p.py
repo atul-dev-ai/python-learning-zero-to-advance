570522aeffc3e65806fffd2860cf23bd8777ec01
atul-dev-ai <paulatul020@gmail.com>
2026-06-20 20:39:39 +0600

    star args

---
+++ part-02-practices/phase-2/p.py
@@ -0,0 +1,15 @@
+def add (a, b, c):
+    return a + b + c
+
+print(add(1, 2, 3))
+
+
+
+# *args likhe ei "star" python ke bole 
+def nums (*numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+print(nums(1, 2, 3, 4, 5))
