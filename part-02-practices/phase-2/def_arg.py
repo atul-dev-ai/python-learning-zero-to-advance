1ed66c783584ce39e1ba0a347a6fe27494692bb0
atul-dev-ai <paulatul020@gmail.com>
2026-06-20 14:34:29 +0600

    def function practice

---
+++ part-02-practices/phase-2/def_arg.py
@@ -0,0 +1,10 @@
+# default argument er value function define er somoy set kora hoy.
+
+# Default argument holo function er parameter er value jodi function call er somoy na deya hoy tahole seta default value hishebe use hobe. 
+
+# Function call korar somoy jodi 
+
+def greet(name = "COder"):
+    print("Hello :", name)
+greet("Atul")
+greet()
