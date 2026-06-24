7c22f9a19bbe6bb311a1ca746b97b02d87a41821
atul-dev-ai <paulatul020@gmail.com>
2026-06-24 12:09:15 +0600

    p.py

--- part-02-practices/phase-2/p.py
+++ part-02-practices/phase-2/p.py
@@ -4,7 +4,7 @@ def add (a, b, c):
 print(add(1, 2, 3))
 
 
-# *args likhe ei "star" python ke bole 
+# *args likhe ei "star" python ke bole je amra onek gula number dite pari, ar oi number gula ke "numbers" variable te store kora hobe. numbers gula ekta tuple hobe, karon *args er maddhome tuple e store hoy.
 def nums (*numbers):
     total = 0
     for number in numbers:
