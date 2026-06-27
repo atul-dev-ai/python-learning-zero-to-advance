cb5ba207665384ba4f3ea8ccc42dc3d481bf3770
atul-dev-ai <paulatul020@gmail.com>
2026-06-27 22:00:49 +0600

    learning square

--- part-02-practices/phase-2/def_arg.py
+++ part-02-practices/phase-2/def_arg.py
@@ -4,7 +4,23 @@
 
 # Function call korar somoy jodi 
 
-def greet(name = "COder"):
+def greet(name = "Coder"):
     print("Hello :", name)
 greet("Atul")
 greet()
+
+
+def add (a, b):
+    return a + b
+print(add(1, 2))
+
+def adds(a, b, c = 5):
+    return a + b + c
+print(adds(1, 2)) # a = 1, b = 2, c = 5(Default value)
+
+print(adds(1, 2, 9)) # a = 1, b = 2, c = 9 (c er value function call er somoy deya hoyeche)
+
+def square(number):
+    return number * number
+print(square(5))
+
