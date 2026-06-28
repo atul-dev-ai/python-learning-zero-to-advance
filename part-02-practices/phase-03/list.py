d64f3afbd32516b93757aa698f51898cb581dee9
atul-dev-ai <paulatul020@gmail.com>
2026-06-28 09:41:33 +0600

    list practice

---
+++ part-02-practices/phase-03/list.py
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry", "Orange", "Fig", "Grape", "Honeydew", "Jackfruit", "Lemon", "Mango", "Nectarine", "Olive", "Papaya", "Quince", "Raspberry", "Strawberry", "Tangerine", "Ugli fruit", "Watermelon"]
+print(fruits)
+print(fruits[0])
+print(fruits[-1])
+print(len(fruits)) # len use kore list er size ber kora jay. ekhane fruits list er size 21.
+print(fruits[0:5]) # ekhane 0 theke 5 porjonto index er value print hobe. ekhane 0, 1, 2, 3, 4 index er value print hobe. ekhane 5 index er value print hobe na.
+print(fruits[5:]) # ekhane 5 theke shuru kore list er shesh porjonto value print hobe. ekhane 0 theke 4 index er value print hobe na. 
+print(fruits[:5])
