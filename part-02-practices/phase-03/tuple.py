33c416603c9a31d740fe6b06ff3bda706fd37017
atul-dev-ai <paulatul020@gmail.com>
2026-06-28 09:33:05 +0600

    tuples

---
+++ part-02-practices/phase-03/tuple.py
@@ -0,0 +1,10 @@
+"""Tuple holeo ekti ordered collection jekhane ekadhik Data rakha jay.
+Tuple er moddhe Data rakha jay kinto seta change kora jay na. tuple er moddhe duplicate value thakte pare. tuple er moddhe mixed date type thakte pare. Parenthesis er moddhe tuple define kora hoy.
+"""
+student = ("Atul Paul", 21, "CIS major AI")
+print(student)
+print(student[0])
+print(student[1])
+print(student[2])
+print(student[-1])
+
