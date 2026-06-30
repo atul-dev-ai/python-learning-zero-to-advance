e3d1e6c3593de64ed0af64cc336a70912925a67f
atul-dev-ai <paulatul020@gmail.com>
2026-06-30 22:15:15 +0600

    value changing practice in list

--- part-02-practices/phase-03/list.py
+++ part-02-practices/phase-03/list.py
@@ -6,3 +6,5 @@ print(len(fruits)) # len use kore list er size ber kora jay. ekhane fruits list 
 print(fruits[0:5]) # ekhane 0 theke 5 porjonto index er value print hobe. ekhane 0, 1, 2, 3, 4 index er value print hobe. ekhane 5 index er value print hobe na.
 print(fruits[5:]) # ekhane 5 theke shuru kore list er shesh porjonto value print hobe. ekhane 0 theke 4 index er value print hobe na. 
 print(fruits[:5])
+fruits[0] = "Avocado" # index er value change kora jay karon list mutable.
+print(fruits)
