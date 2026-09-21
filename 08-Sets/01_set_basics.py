6c81d133eea78d04d2537f9580d2ec7a575a11e6
atul-dev-ai <paulatul020@gmail.com>
2026-09-21 22:25:24 +0600

    structure changed

---
+++ 08-Sets/01_set_basics.py
@@ -0,0 +1,36 @@
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+print(type(my_set)) # class = SET
+
+student_ids = [101, 102, 103, 104, 105, 101, 102]
+unique_ids = set(student_ids)
+print(unique_ids)
+
+# integer set
+numbers = {1, 2, 3, 4, 5}
+print(type(numbers)) # set
+print(numbers)
+
+# string set
+fruits = {"Apple", "Banana", "Mango"}
+print(fruits)
+
+# Mixed data
+data = {19, "Python", 3.5, True}
+print(data)
+
+# Empty set
+empty_set = {}
+print(type(empty_set)) # {} ekti empty dictionary create kore
+
+empty_set_01 = set()
+print(type(empty_set_01)) # Class = SET
+
+names = ["Atul", "Rahim", "Atul", "Karim", "Rahim"]
+print(type(names)) # list
+print(names)
+unique_names = set(names) # Set
+print(unique_names)
+
+for number in numbers:
+    print(number)
