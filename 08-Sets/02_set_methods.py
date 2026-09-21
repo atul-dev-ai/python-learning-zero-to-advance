24dfbd6afd7b3524a4c7ad1d702873da172fb3c5
atul-dev-ai <paulatul020@gmail.com>
2026-09-21 22:11:49 +0600

    set methods practice

---
+++ 08-Sets/02_set_methods.py
@@ -0,0 +1,14 @@
+numbers = {10, 20, 30}
+
+numbers.add(40)
+print(numbers)
+
+numbers.update([40, 50, 60, 70])
+print(numbers)
+
+numbers.discard(20)
+print(numbers)
+
+numbers.remove(30)
+print(numbers)
+
