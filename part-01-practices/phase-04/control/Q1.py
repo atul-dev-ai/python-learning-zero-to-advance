99c43daf0f2c80db95b7b100894ebf49033b0e63
atul-dev-ai <paulatul020@gmail.com>
2026-09-21 19:35:57 +0600

    Organize folder structure for Python learning

--- part-01-practices/phase-04/control/Q1.py
+++
@@ -1,7 +0,0 @@
-# even or odd
-num = int(input('Enter a number: '))
-
-if num % 2 == 0:
-    print("Even", num)
-else:
-    print("Odd", num)
--- part-01-practices/phase-04/control/Q2.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter a number : "))
-if num > 0:
-    print("+ve", num)
-elif num < 0:
-    print("-ve", num)
-else:
-    print("==0", num)
