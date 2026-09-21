0746bd7b601e5bdf4391d01f31c5b9762104e964
atul-dev-ai <paulatul020@gmail.com>
2026-09-21 21:51:58 +0600

    email practice

---
+++ 08-Sets/unique_email_practice.py
@@ -0,0 +1,11 @@
+emails = [
+    "atul@gmail.com",
+    "Rahim@gmail.com",
+    "atul@gmail.com",
+    "karim@gmail.com",
+    "sakib@gmail.com"
+]
+print(type(emails)) # list
+unique_emails = set(emails)
+print(unique_emails)
+print(type(unique_emails)) # set
