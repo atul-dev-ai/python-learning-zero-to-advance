80ea4d46b464fe2782a58be65571fa5a9d498203
atul-dev-ai <paulatul020@gmail.com>
2026-09-21 19:36:17 +0600

    Organize folder structure for Python learning

--- part-01-practices/zip-unzip/zip.py
+++
@@ -1,8 +0,0 @@
-names = ['Atul', 'Ankit', 'Anik']
-marks = [90, 80, 70]
-
-for i in range(len(names)):
-    print(names[i], marks[i]);
-
-for name, mark in zip(names, marks):
-    print(name, mark)
--- part-02-practices/phase-01/ques.py
+++
@@ -1,9 +0,0 @@
-def check_result(marks):
-    if marks >= 33:
-        return "Pass"
-    else:
-        return "Fail"
-    
-marks = int(input("Enter your marks: "))
-result = check_result(marks)
-print(result)
