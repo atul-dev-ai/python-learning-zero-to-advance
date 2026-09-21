760fc984e3724f46a04a4f8ef391a1392311bf28
atul-dev-ai <paulatul020@gmail.com>
2026-09-21 19:49:10 +0600

    tuple integer set

---
+++ 08-Sets/01_tuple_basics.py
@@ -0,0 +1,13 @@
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+print(type(my_set)) # class = SET
+
+student_ids = [101, 102, 103, 104, 105, 101, 102]
+unique_ids = set(student_ids)
+print(unique_ids)
+
+# data = {[1, 2], [3, 4]}  type error
+
+# integer set
+numbers = {1, 2, 3, 4, 5}
+print(numbers)
