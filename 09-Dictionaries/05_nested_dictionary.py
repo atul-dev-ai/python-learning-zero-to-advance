664670003637b9ec62cb67187b5cbe4a793a1191
atul-dev-ai <paulatul020@gmail.com>
2026-09-22 22:40:51 +0600

    nested dictionary

---
+++ 09-Dictionaries/05_nested_dictionary.py
@@ -0,0 +1,20 @@
+students = {
+    "student1": {
+        "name": "Atul Paul",
+        "age": 21,
+        "marks": 85
+    },
+    "student2": {
+        "name": "Ankit paul",
+        "age": 9,
+        "marks": 78
+    }
+}
+
+# data access
+print(students["student1"]["name"])
+print(students["student1"]["marks"])
+
+print(students["student2"]["age"])
+
+# nested dictionary update
