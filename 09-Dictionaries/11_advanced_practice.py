f50f8e4c6883948c0da1e0d0cf2418a711decd82
atul-dev-ai <paulatul020@gmail.com>
2026-09-25 18:04:01 +0600

    practice 01 Nested Dictionary

---
+++ 09-Dictionaries/11_advanced_practice.py
@@ -0,0 +1,23 @@
+# practice - 01
+students = {
+    "student1": {
+        "name": "Atul Paul",
+        "age": 22,
+        "marks": 85
+    },
+    "student2": {
+       "name": "Ankit Paul",
+       "age": 9,
+       "marks": 99
+    },
+    "student3": {
+        "name": "Karim",
+        "age": 21,
+        "marks": 92
+    }
+}
+
+print(students["student1"]["name"])
+print(students["student2"]["age"])
+print(students["student3"]["marks"])
+
